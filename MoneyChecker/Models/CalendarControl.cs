328104796fd795458ef84e64d96fb8a933bddcca
Viktor Velychko <Cezn.Vi@gmail.com>
2023-05-29 22:57:53 +0300

    WIP

--- MoneyChecker/Models/CalendarControl.cs
+++ MoneyChecker/Models/CalendarControl.cs
@@ -14,6 +14,7 @@ namespace MoneyChecker.Models
         private DateTime _curDate = new DateTime();
         private List<CalendarCell> _cells;
 
+        /* /////-----------   Конструктор   -----------\\\\\ */
         public CalendarControl(ListView ListViewCalendar)
         {
             _curDate = DateTime.Now;
@@ -21,6 +22,56 @@ namespace MoneyChecker.Models
             Init();
         }
 
+        /* /////-----------   Свойства   -----------\\\\\ */
+        public string GetCurentDatePeriod
+        {
+            get 
+            {
+                return _curDate.ToString("MM yyyy");
+            }
+        }
+
+        /* /////-----------   Методы   -----------\\\\\ */
+
+        /// <summary>
+        /// Добавить Месяц к текущей дате
+        /// </summary>
+        public void AddMonth()
+        {
+            _curDate = _curDate.AddMonths(1);
+            Init();
+        }
+        
+        /// <summary>
+        /// Отнять Месяц от текущей даты
+        /// </summary>
+        public void MinusMonth()
+        {
+            _curDate = _curDate.AddMonths(-1);
+            Init();
+        }
+
+        /// <summary>
+        /// Добавить Год к текущей дате
+        /// </summary>
+        public void AddYear()
+        {
+            _curDate = _curDate.AddYears(1);
+            Init();
+        }
+
+        /// <summary>
+        /// Отнять Год от текущей даты
+        /// </summary>
+        public void MinusYear()
+        {
+            _curDate = _curDate.AddYears(-1);
+            Init();
+        }
+
+        /// <summary>
+        /// Инициализация ячеек календаря
+        /// </summary>
         private void Init()
         {
             int _days = DateTime.DaysInMonth(_curDate.Year, _curDate.Month);
@@ -28,42 +79,98 @@ namespace MoneyChecker.Models
             _cells = new List<CalendarCell>();
             CalendarCell cell;
 
+            
+
+
             for (int i = 0; i < _days; i++)
             {
-               cell  = new CalendarCell() { _date = new DateTime(_curDate.Year, _curDate.Month, i + 1) };
+                cell = new CalendarCell() { _date = new DateTime(_curDate.Year, _curDate.Month, i + 1) };
 
                 _cells.Add(cell);
             }
 
             _listView.ItemsSource = _cells;
 
-            foreach (var item in _listView.ItemsPanel.Resources.OfType<Grid>())
-            {
-                if(item is Grid) 
-                {
-                    item.Background = System.Windows.Media.Brushes.Red;
-                
-                }
-            }
-
         }
 
+        /// <summary>
+        /// Добавить дней перед первым числом в зависимости от дня недели первого числа
+        /// </summary>
+        private void AddCellsBeforeFirstDay()
+        {
+            DateTime firstDayOfMonth = new DateTime(_curDate.Year, _curDate.Month, 1);
+            
+            DateTime monthBefore = new DateTime(_curDate.Year, _curDate.AddMonths(-1).Month, 1);
+            int _days = DateTime.DaysInMonth(monthBefore.Year, monthBefore.Month);
 
+            if (firstDayOfMonth.DayOfWeek == DayOfWeek.Monday)
+            {
+                return;
+            }
+            else
+            {
+                CalendarCell cell;
+
+                for (int i = _days - Convert.ToInt32(firstDayOfMonth.DayOfWeek); i < _days; i++) 
+                {
+                    cell = new CalendarCell() { _date = new DateTime(monthBefore.Year, monthBefore.Month, _days + i) };
+                    _cells.Add(cell);
+                }
+                
+            }
+
+
+
+
+
+
+          
+
+
+        }
     }
 
 
     public class CalendarCell
     {
+
+        /* /////-----------   Переменные   -----------\\\\\ */
+        /// <summary>
+        /// Хранит дату
+        /// </summary>
         public DateTime _date;
 
 
+        /* /////-----------   Свойства   -----------\\\\\ */
+
+        /// <summary>
+        /// Возвращает дату в виде строки
+        /// </summary>
         public string Date { get { return _date.Day.ToString(); } }
 
+        /// <summary>
+        /// Возвращает строку с именем дня недели
+        /// </summary>
         public string DayOfWeek { get { return _date.ToString("dddd"); } }
 
-        public DayOfWeek DayOfWeekEnum { get { return _date.DayOfWeek; } }
+        /// <summary>
+        /// Приводит день недели к системному Enum
+        /// </summary>
+        private DayOfWeek DayOfWeekEnum { get { return _date.DayOfWeek; } }
 
-        public Color CellColor { get { } }
+        /// <summary>
+        /// Возвращает цвет ячейки в виде строки (для выходных дней другой цвет)
+        /// </summary>
+        public string CellColor
+        { 
+            get 
+            {
+                if(this.DayOfWeekEnum == System.DayOfWeek.Saturday || this.DayOfWeekEnum == System.DayOfWeek.Sunday)
+                    return Color.Red.Name;
+                else 
+                    return Color.White.Name;
+            } 
+        }
 
     }
 
--- MoneyChecker/Views/CalendarPage.xaml.cs
+++ MoneyChecker/Views/CalendarPage.xaml.cs
@@ -14,9 +14,31 @@ namespace MoneyChecker.Views
         {
             InitializeComponent();
             calendarControl = new CalendarControl(ListViewCalendar);
+            LabelCurentDate.Content = calendarControl.GetCurentDatePeriod;
         }
 
+        private void ButtonPlusMonth_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            calendarControl.AddMonth();
+            LabelCurentDate.Content = calendarControl.GetCurentDatePeriod;
+        }
 
+        private void ButtonMinusMonth_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            calendarControl.MinusMonth();
+            LabelCurentDate.Content = calendarControl.GetCurentDatePeriod;
+        }
 
+        private void ButtonPlusYear_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            calendarControl.AddYear();
+            LabelCurentDate.Content = calendarControl.GetCurentDatePeriod;
+        }
+
+        private void ButtonMinusYear_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            calendarControl.MinusYear();
+            LabelCurentDate.Content = calendarControl.GetCurentDatePeriod;
+        }
     }
 }
